826f091230eef234cadc66dfff08c4d686e8660c
srijanrana77 <70372578+srijanrana77@users.noreply.github.com>
2021-05-19 12:00:51 +0530

    backend of admin pages is completed

---
+++ src/main/java/com/example/Project/Integrated/Project/model/Role.java
@@ -0,0 +1,24 @@
+//package com.example.Project.Integrated.Project.model;
+//
+//import lombok.Data;
+//
+//import javax.persistence.*;
+//import javax.validation.constraints.NotEmpty;
+//import java.util.List;
+//
+//@Data
+//@Entity
+//@Table(name = "roles")
+//public class Role {
+//
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.AUTO)
+//    private Integer id;
+//
+//    @Column(nullable = false, unique = true)
+//    @NotEmpty
+//    public String name;
+//
+//    @ManyToMany(mappedBy = "roles")
+//    private List<User> users;
+//}
