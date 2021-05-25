cd319d284b2eae4bd3ef62c0a0d53fbe136280c5
ujjwal1206 <ujjwal1985.cse18@chitkara.edu.in>
2021-05-25 10:18:11 +0530

    some updations in googleauth

--- src/main/java/com/example/Project/Integrated/Project/controller/LoginController.java
+++ src/main/java/com/example/Project/Integrated/Project/controller/LoginController.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -33,16 +34,5 @@ public class LoginController {
     @GetMapping("/register")
     public String registerGet(){
         return "register";
-    }
-    @PostMapping("/register")
-    public String registerPost(@ModelAttribute("user") User user, HttpServletRequest request) throws ServletException {
-        String password=user.getPassword();
-        user.setPassword(bCryptPasswordEncoder.encode(password));
-        List<Role> roles=new ArrayList<>();
-        roles.add(roleRepository.findById(2).get());
-        user.setRoles(roles);
-        userRepository.save(user);
-        request.login(user.getEmail(), password);
-        return "redirect:/";
     }
 }
