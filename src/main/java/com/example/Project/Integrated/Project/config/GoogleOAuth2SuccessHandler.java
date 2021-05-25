dc90b636a46a5de76f0872d2a0940c84011b3f17
ujjwal1206 <ujjwal1985.cse18@chitkara.edu.in>
2021-05-25 08:21:34 +0530

    Added some updations in googleauth

--- src/main/java/com/example/Project/Integrated/Project/config/GoogleOAuth2SuccessHandler.java
+++ src/main/java/com/example/Project/Integrated/Project/config/GoogleOAuth2SuccessHandler.java
@@ -12,6 +12,7 @@ import org.springframework.security.web.RedirectStrategy;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
+import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
